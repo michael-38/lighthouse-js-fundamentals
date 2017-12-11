62405a72536fea448dfbf2158bbe352fb5c9ec0f
Michael <michael.38@outlook.com>
2017-12-11 19:33:15 +0000

    Adds conditionals exercises

---
+++ howManyHundreds.js
@@ -0,0 +1,16 @@
+function howManyHundreds (number) {
+  var remainder = number % 100
+  if (remainder > 0) {
+    return ((number - remainder)/100);
+  }
+  return number / 100;
+}
+
+
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
